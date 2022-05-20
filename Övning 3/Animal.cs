37245cdb12e14914409614c34e72867259addfdf
emilks <emil.keski@gmail.com>
2022-05-20 16:26:20 +0200

    Mer Polymorfism

--- Övning 3/Animal.cs
+++ Övning 3/Animal.cs
@@ -4,29 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//3.3.13) Om vi vill ha ett attribut för alla fåglar ska vi lägga det i Bird klassen
-//3.3.14) Om vi vill ha ett attribut som gäller alla djur borde det vara i Animal klassen
-
 namespace Övning_3
 {
     public abstract class Animal
     {
-        private string name;
-        private double weight;
-        private int age;
+        internal string name { get; set; }
+        internal double weight;
+        internal int age;
+        internal string type;
 
         public abstract string DoSound();
+
+        public abstract string Stats();
+
+        public Animal(string Name, double Weight, int Age) {
+            name = Name;
+            weight = Weight;
+            age = Age;
+        }
     }
 
     public class Horse: Animal
     {
+
         public double RunningSpeed;
         public override string DoSound()
         {
             return "neigh";
         }
-    }
 
+        public override string Stats()
+        {
+            return $"Type: {type}, Name: {name}, Weight: {weight}, Age: {age}, RunningSpeed: {RunningSpeed}";
+        }
+
+        public Horse(string Name, double Weight, int Age) : base(Name, Weight, Age) { type = "horse";  RunningSpeed = 3.5; }
+    }
+    
     public class Dog : Animal
     {
         public bool Tame;
@@ -34,15 +48,34 @@ namespace Övning_3
         {
             return "bark";
         }
-    }
+        public override string Stats()
+        {
+            return $"Type: {type}, Name: {name}, Weight: {weight}, Age: {age}, Tame: {Tame}";
+        }
 
+        public string Runs()
+        {
+            return $"{name} runs very fast";
+        }
+
+        public Dog(string Name, double Weight, int Age) : base(Name, Weight, Age) { type = "dog"; Tame = true; }
+
+    }
+    
     public class Hedgehog : Animal
     {
         public int NrOfSpikes;
         public override string DoSound()
         {
-            return "squek";
+            return "squeek";
         }
+        public override string Stats()
+        {
+            return $"Type: {type}, Name: {name}, Weight: {weight}, Age: {age}, Number of spikes: {NrOfSpikes}";
+        }
+
+        public Hedgehog(string Name, double Weight, int Age) : base(Name, Weight, Age) { type = "hedgehog"; NrOfSpikes = 55; }
+
     }
 
     public class Worm : Animal
@@ -52,6 +85,13 @@ namespace Övning_3
         {
             return "none";
         }
+        public override string Stats()
+        {
+            return $"Type: {type}, Name: {name}, Weight: {weight}, Age: {age}, Poisonous: {IsPoisonous}";
+        }
+
+        public Worm(string Name, double Weight, int Age) : base(Name, Weight, Age) { type = "worm"; IsPoisonous = true; }
+
     }
     public class Bird : Animal
     {
@@ -61,6 +101,13 @@ namespace Övning_3
         {
             return "tweet";
         }
+        public override string Stats()
+        {
+            return $"Type: {type}, Name: {name}, Weight: {weight}, Age: {age}, Wingspan: {Wingspan}";
+        }
+
+        public Bird(string Name, double Weight, int Age) : base(Name, Weight, Age) { type = "bird"; Wingspan = 150; }
+
     }
     public class Wolf : Animal
     {
@@ -69,33 +116,70 @@ namespace Övning_3
         {
             return "howl";
         }
+        public override string Stats()
+        {
+            return $"Type: {type}, Name: {name}, Weight: {weight}, Age: {age}, Size of Pack: {SizeOfPack}";
+        }
+
+        public Wolf(string Name, double Weight, int Age) : base(Name, Weight, Age) { type = "wolf"; SizeOfPack = 9; }
+
     }
 
     public class Pelican : Bird
     {
         public int BeakLength;
+
+        public override string Stats()
+        {
+            return $"Type: {type}, Name: {name}, Weight: {weight}, Age: {age}, Wingspan: {Wingspan}, Beak Length: {BeakLength}";
+        }
+
+        public Pelican(string Name, double Weight, int Age) : base(Name, Weight, Age) { type = "pelican"; Wingspan = 150;  BeakLength = 45; }
+
     }
 
     public class Flamingo : Bird
     {
         public string color;
+        public override string Stats()
+        {
+            return $"Type: {type}, Name: {name}, Weight: {weight}, Age: {age}, Wingspan: {Wingspan}, Color: {color}";
+        }
+
+        public Flamingo(string Name, double Weight, int Age) : base(Name, Weight, Age) { type = "flamingo"; Wingspan = 150; color = "Pink"; }
+
     }
 
     public class Swan : Bird
     {
-        public int NrOfChildren;
-    }
+        public int nrOfChildren; 
+        public override string Stats()
+        {
+            return $"Type: {type}, Name: {name}, Weight: {weight}, Age: {age}, Wingspan: {Wingspan}, Number of Children: {nrOfChildren}";
+        }
 
+        public Swan(string Name, double Weight, int Age) : base(Name, Weight, Age) { type = "swan"; Wingspan = 150; nrOfChildren = 2; }
+
+    }
     public class Wolfman : Animal, IPerson
     {
+
         public override string DoSound()
         {
             return "howl";
+        }
+        public override string Stats()
+        {
+            return $"Type: {type}, Name: {name}, Weight: {weight}, Age: {age}";
         }
 
         public string Talk()
         {
             return "I'm the big bad wolf";
         }
+
+        public Wolfman(string Name, double Weight, int Age) : base(Name, Weight, Age) { type = "wolfman"; }
+    
     }
+    
 }
--- Övning 3/Program.cs
+++ Övning 3/Program.cs
@@ -1,5 +1,13 @@
 ﻿using System;
 
+//3.3.13) Om vi vill ha ett attribut för alla fåglar ska vi lägga det i Bird klassen
+//3.3.14) Om vi vill ha ett attribut som gäller alla djur borde det vara i Animal klassen
+
+//3.4.9) Dog och Horse må båda ärva av den abstrakta klassen Animal men en Horse är inte en Dog
+//3.4.10) För att kunna lagra alla klasserna i samma lista så måste listan vara av klassen Animal efter som alla andra klasser baseras på den
+//3.4.13) Den använder alltid den metoden med override för den specifika klassen
+//3.4.17) Eftersom inte metoden finns deklarerad i Animal så kan vi inte komma åt den
+
 namespace Övning_3
 {
     internal class Program
@@ -39,8 +47,48 @@ namespace Övning_3
                 Console.WriteLine(error.UEMessage());
             }
 
-            Wolfman werewolf = new Wolfman();
-            Console.WriteLine($"The wolfman {werewolf.DoSound()} or says \"{werewolf.Talk()}\"");
+            Wolfman werewolf = new Wolfman("Big Bad Wolf", 60, 35);
+            //Console.WriteLine($"The wolfman {werewolf.DoSound()} or says \"{werewolf.Talk()}\"");
+
+            List<Animal> animals = new List<Animal>();
+            animals.Add(werewolf);
+            animals.Add(new Dog("Lassie", 30, 6));
+            animals.Add(new Horse("Bucephalus", 150, 12));
+            animals.Add(new Pelican("Birdie", 15, 8));
+            animals.Add(new Bird("Polly", 12, 4));
+            animals.Add(new Worm("Dune", 0.5, 1));
+
+            foreach(var animal in animals)
+            {
+                Console.WriteLine($"The {animal.type} makes the sound: {animal.DoSound()}");
+                if(animal is IPerson)
+                {
+                    IPerson animalPerson = (IPerson)animal;
+                    Console.WriteLine($"It also says: {animalPerson.Talk()}");
+                }
+            }
+
+            List<Dog> dogs = new List<Dog>();
+            //dogs.Add(new Horse("Bucephalus", 150, 12)); //Funkar inte
+
+            Console.WriteLine("\n Stats för alla djur i listan:");
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal.Stats());
+
+            }
+            Console.WriteLine("\n Stats för hundar i listan:");
+            foreach (var animal in animals)
+            {
+                if (animal is Dog)
+                {
+                    Console.WriteLine(animal.Stats());
+                    //animal.Run();  //Funkar inte
+                    Dog dog = animal as Dog;
+                    Console.WriteLine(dog.Runs());
+                    
+                }
+            }
         }
     }
 }
